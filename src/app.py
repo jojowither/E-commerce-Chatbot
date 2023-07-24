eca283071a1207b598e3b10b9ff87f83e03275af
jojowither <ms0421555@gmail.com>
2023-07-24 09:55:57 +0000

    feat: upload code

---
+++ src/app.py
@@ -0,0 +1,59 @@
+import os
+from dotenv import load_dotenv, dotenv_values 
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.vectorstores import FAISS
+from langchain import HuggingFaceHub
+
+
+config = dotenv_values("../.env") 
+os.environ['OPENAI_API_KEY'] = config['OPENAI_API_KEY'] 
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = config['HUGGINGFACEHUB_API_TOKEN']
+file_path = '../data/se_product.csv'
+vectorstore_path = '../faiss_product/'
+app = FastAPI()
+
+
+# os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+# embeddings = HuggingFaceEmbeddings()
+# llm=HuggingFaceHub(repo_id="google/flan-t5-xl", model_kwargs={"temperature":0, "max_length":512})
+# chain = load_qa_chain(llm, chain_type="stuff")
+
+embeddings = OpenAIEmbeddings()
+
+if not os.path.exists(vectorstore_path):
+    loader = CSVLoader(file_path=file_path, encoding="utf-8", csv_args={'delimiter': ','})
+    data = loader.load()
+    vectorstore = FAISS.from_documents(data, embeddings)
+    vectorstore.save_local(vectorstore_path)
+    print('Finsih save vectorstore')
+
+else:
+    vectorstore = FAISS.load_local(vectorstore_path, embeddings)
+
+llm = ChatOpenAI(temperature=0.0, model_name='gpt-3.5-turbo')
+chain = ConversationalRetrievalChain.from_llm(llm=llm, 
+                                              retriever=vectorstore.as_retriever())   
+
+
+class ChatModel(BaseModel):
+    question: str
+    chat_history: List
+
+
+@app.post("/conversation")
+async def conversation(data: ChatModel):
+    response = chain({"question": data.question, "chat_history": data.chat_history})
+    return response
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ src/frontend.py
@@ -0,0 +1,48 @@
+import streamlit as st
+from streamlit_chat import message
+import requests
+
+url = "http://localhost:8000/conversation"
+
+def conversational_chat(query):
+    request_json = {"question": query, "chat_history": st.session_state['history']}
+    response = requests.post(url, json=request_json)
+    response = response.json()
+    
+    st.session_state['history'].append((query, response["answer"]))
+    
+    return response["answer"]
+
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["Hello ! Ask me anything about products 🤗"]
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = ["Hey ! 👋"]
+    
+#container for the chat history
+response_container = st.container()
+#container for the user's text input
+container = st.container()
+
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        
+        user_input = st.text_input("Query:", placeholder="Talk about your csv data here (:", key='input')
+        submit_button = st.form_submit_button(label='Send')
+        
+    if submit_button and user_input:
+        output = conversational_chat(user_input)
+        
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
+
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
+            message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
+            
+#streamlit run frontend.py
